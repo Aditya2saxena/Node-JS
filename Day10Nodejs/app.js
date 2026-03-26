cc79708a6de3de4e0bcc1443c495c91a01a86564
Aditya <officaladityasaxena@gmail.com>
2026-03-26 15:44:18 +0530

    learn about jwt token

--- Day10Nodejs/app.js
+++ Day10Nodejs/app.js
@@ -1,10 +1,33 @@
 const express = require("express");
 
 const userRoute = require("./routes/userroute.js");
+const authRoute = require("./routes/authRoute");
 const dotenv = require("dotenv");
+const jwt=require("jsonwebtoken");
 dotenv.config();
 
 const app = express();
+
+
+const authMiddleware=(req,res,next)=>{
+    const Token=req.headers.authorization?.split(" ")[1];
+    if(!Token){
+       return res.send("Token not found");
+    }
+
+    try{
+        decoded=kwt .verify(Token,jwtToken)
+        console.log(decoded);
+        req.user=decoded;
+        next();
+
+    }catch(err){
+        res.send("Token is not valid");
+
+    }
+
+}
+app.use("/get", authMiddleware, userRoute);
 
 const customMiddleware = (req, res, next) => {
     if (req.query.skip === "true") {
@@ -19,5 +42,9 @@ app.use(customMiddleware);
 
 app.use('/get', userRoute);
 console.log(userRoute);
+
+
+
+app.use("/login", authRoute);
 
 module.exports = app;
---
+++ Day10Nodejs/controllers/authController.js
@@ -0,0 +1,12 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const jwtToken = process.env.JWT_TOKEN;
+
+exports.generateToken = (req, res) => {
+    const { username, email } = req.query;
+
+    const token = jwt.sign({ username, email }, jwtToken);
+
+    res.json({ token });
+};
